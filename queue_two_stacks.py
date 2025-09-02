90b42f3cb976e3e4218e0eaaa71cba2163c17c10
Wei Hang, Zheng <zhengw@fortinet.com>
2025-09-02 12:48:39 -0700

    2s

--- queue_two_stacks.py
+++ queue_two_stacks.py
@@ -1,24 +1,38 @@
 class Queue:
     def __init__(self):
-        self.stack1 = []  # for enqueue
-        self.stack2 = []  # for dequeue
+        self.stack1 = []  # used for enqueue
+        self.stack2 = []  # used for dequeue
+        self.size_ = 0
 
     def enqueue(self, val):
-        # TODO: Implement
-        pass
+        # Always push to stack1
+        self.stack1.append(val)
+        self.size_ += 1
 
     def dequeue(self):
-        # TODO: Implement
-        pass
+        if self.is_empty():
+            raise IndexError("Queue is empty")
+
+        # If stack2 is empty, move everything from stack1
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+
+        self.size_ -= 1
+        return self.stack2.pop()
 
     def peek(self):
-        # TODO: Implement
-        pass
+        if self.is_empty():
+            raise IndexError("Queue is empty")
+
+        if not self.stack2:
+            while self.stack1:
+                self.stack2.append(self.stack1.pop())
+
+        return self.stack2[-1]
 
     def is_empty(self):
-        # TODO: Implement
-        pass
+        return self.size_ == 0
 
     def get_size(self):
-        # TODO: Implement
-        pass
+        return self.size_
