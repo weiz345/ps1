17803b63a4f1c240e5e31ba11b63013e8d798b9d
Wei Hang, Zheng <zhengw@fortinet.com>
2025-09-02 13:10:20 -0700

    3s

--- three_sum.py
+++ three_sum.py
@@ -1,4 +1,23 @@
 class Solution:
     def three_sum(self, nums):
-        # TODO: Implement
-        pass
+        nums.sort()
+        n = len(nums)
+        result = []
+        seen_triplets = set()
+
+        for i in range(n):
+            seen = {}
+            for j in range(i + 1, n):
+                complement = -nums[i] - nums[j]
+                if complement in seen:
+                    triplet = (nums[i], complement, nums[j])  # tuple for set
+                    if triplet not in seen_triplets:
+                        seen_triplets.add(triplet)
+                        result.append(list(triplet))
+                seen[nums[j]] = True
+
+        return result
+
+sol = Solution()
+print(sol.three_sum([-1, 0, 1, 2, -1, -4]))
+# Output: [[-1, -1, 2], [-1, 0, 1]]
