643c4666304ffc6ace2c3551ae7c19452d0cf921
Wei Hang, Zheng <zhengw@fortinet.com>
2025-09-02 12:39:27 -0700

    q

--- queue_linked_list.py
+++ queue_linked_list.py
@@ -5,17 +5,31 @@ class ListNode:
 
 class Queue:
     def __init__(self):
-        self.front = None
-        self.rear = None
-        self.size = 0
+        self.front = None   # first node
+        self.rear = None    # last node
+        self.size = 0       # number of items
 
     def enqueue(self, val):
-        # TODO: Implement
-        pass
+        new_node = ListNode(val)
+        if self.is_empty():
+            # both front and rear point to new node
+            self.front = self.rear = new_node
+        else:
+            # attach new node at rear and update rear
+            self.rear.next = new_node
+            self.rear = new_node
+        self.size += 1
 
     def dequeue(self):
-        # TODO: Implement
-        pass
+        if self.is_empty():
+            raise IndexError("Queue is empty")
+        value = self.front.val
+        self.front = self.front.next
+        # if queue becomes empty, rear must also be None
+        if self.front is None:
+            self.rear = None
+        self.size -= 1
+        return value
 
     def peek(self):
         if self.is_empty():
@@ -23,10 +37,7 @@ class Queue:
         return self.front.val
 
     def is_empty(self):
-        # TODO: Implement
-        pass
+        return self.size == 0
 
     def get_size(self):
         return self.size
-    
-    
