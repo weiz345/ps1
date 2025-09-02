57c9b8be71fe7aff1f3e43b2069128d8b2dfe2d1
Wei Hang, Zheng <zhengw@fortinet.com>
2025-09-02 13:03:35 -0700

    bsearch

--- bin_search.py
+++ bin_search.py
@@ -1,4 +1,14 @@
 class Solution:
     def binary_search(self, arr, target):
-        # TODO: Implement
-        pass
+        left, right = 0, len(arr) - 1
+
+        while left <= right:
+            mid = (left + right) // 2
+            if arr[mid] == target:
+                return mid
+            elif arr[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+
+        return -1  # not found
