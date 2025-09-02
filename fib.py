0b98707afbacbcbdf7bb3cd3b21fc703c100fdd0
Wei Hang, Zheng <zhengw@fortinet.com>
2025-09-02 12:57:19 -0700

    fib

--- fib.py
+++ fib.py
@@ -1,8 +1,22 @@
 class Solution:
     def fib_recursive(self, n):
-        # TODO: Implement
-        pass
+        # helper recursive function for nth Fibonacci number
+        def fib(k):
+            if k == 0:
+                return 0
+            elif k == 1:
+                return 1
+            return fib(k - 1) + fib(k - 2)
+
+        result = []
+        for i in range(n):
+            result.append(fib(i))
+        return result
 
     def fib_iterative(self, n):
-        # TODO: Implement
-        pass
+        result = []
+        a, b = 0, 1
+        for i in range(n):
+            result.append(a)
+            a, b = b, a + b
+        return result
