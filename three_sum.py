1d8cbaf3496814fee99c35e26f6f9cd02e60972b
Wei Hang, Zheng <zhengw@fortinet.com>
2025-09-02 13:20:11 -0700

    3s

--- three_sum.py
+++ three_sum.py
@@ -6,18 +6,28 @@ class Solution:
         seen_triplets = set()
 
         for i in range(n):
-            seen = {}
             for j in range(i + 1, n):
-                complement = -nums[i] - nums[j]
-                if complement in seen:
-                    triplet = (nums[i], complement, nums[j])  # tuple for set
-                    if triplet not in seen_triplets:
-                        seen_triplets.add(triplet)
-                        result.append(list(triplet))
-                seen[nums[j]] = True
+                complement = -(nums[i] + nums[j])
 
+                # binary search for complement in nums[j+1:]
+                left, right = j + 1, n - 1
+                while left <= right:
+                    mid = (left + right) // 2
+                    if nums[mid] == complement:
+                        triplet = (nums[i], nums[j], nums[mid])
+                        if triplet not in seen_triplets:
+                            seen_triplets.add(triplet)
+                            result.append(list(triplet))
+                        break
+                    elif nums[mid] < complement:
+                        left = mid + 1
+                    else:
+                        right = mid - 1
+
+        # final lexicographic order
         return result
-
+    
 sol = Solution()
 print(sol.three_sum([-1, 0, 1, 2, -1, -4]))
 # Output: [[-1, -1, 2], [-1, 0, 1]]
+
